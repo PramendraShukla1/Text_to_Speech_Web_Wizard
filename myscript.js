a5deb195991fc08103d1be3707b26cd7cdf2f83d
PramendraShukla1 <shuklapramendra99@gmail.com>
2023-03-24 00:38:35 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,11 @@
+﻿document.getElementById('id_speech').onclick = () => {
+  let lang =document.getElementById('id_lang').value;
+  let rate=document.getElementById('id_rate').value;
+  let text =document.getElementById('id_text').value;
+
+  let option = {};
+  option['lang'] = lang;
+  option['rate'] = parseFloat(rate);
+
+  chrome.tts.speak(text, option);
+}
